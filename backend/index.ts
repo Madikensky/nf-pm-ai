e4848f71f96db361aeb5b8d4aa3341d8a4299b09
Madiyar <madikensky2003@gmail.com>
2024-06-27 11:36:24 +0500

    feat: create chat

---
+++ backend/index.ts
@@ -0,0 +1,43 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import Trello from 'trello';
+import { config } from 'dotenv';
+import { Request, Response } from 'express';
+
+const PORT = 5000 || process.env.PORT;
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+config();
+
+const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+
+app.post('/gemini', async (req: Request, res: Response) => {
+  try {
+    const model = genAi.getGenerativeModel({ model: 'gemini-1.5-pro' });
+    const { userPrompt, history } = req.body;
+
+    const apiKey = process.env.TRELLO_API as string;
+    const token = process.env.TRELLO_AUTH_TOKEN as string;
+
+    const trello = new Trello(apiKey, token);
+
+    const getAllBoards = async () => {};
+
+    const chat = model.startChat({ history });
+    const sendPromptToGemini = await chat.sendMessage(userPrompt);
+    const geminiAnswer = sendPromptToGemini.response.text();
+    res.send(geminiAnswer);
+  } catch (e) {
+    res.status(500).send({ message: e });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
