aec8626336f9dd9007e2c1eec4d4e4ce811063ab
Madiyar <madikensky2003@gmail.com>
2024-07-09 15:11:42 +0500

    add cursor  on login page

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header({ showLogin }: LoginProps) {
     <header className="flex border-red-900 p-3 items-center justify-between px-6 fixed w-full top-0 left-0">
       <div className="flex items-center justify-center">
         <Image src="/logo_taskify.svg" alt="logo" width={40} height={40} />
-        <p className="header-text">Taskify.AI</p>
+        <p className="header-text cursor-default">Taskify.AI</p>
       </div>
       {showLogin && <button className="header-text">Войти</button>}
     </header>
