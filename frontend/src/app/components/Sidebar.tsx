a0a4675374a94d2b636eec124bab13022823e436
Madiyar <madikensky2003@gmail.com>
2024-07-17 23:11:30 +0500

    feat: add sidebar functionality

--- frontend/src/app/components/Sidebar.tsx
+++ frontend/src/app/components/Sidebar.tsx
@@ -5,12 +5,15 @@ import Loading from './Loading';
 export default function SideBar({ isOpen, setIsOpen }: any) {
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const [boards, setBoards] = useState<any[]>([]);
+  const [openIframe, setOpenIframe] = useState(false);
+  const [currentBoard, setCurrentBoard] = useState('');
 
   useEffect(() => {
     if (localStorage.getItem('trelloBoards')) {
       setBoards(JSON.parse(localStorage.getItem('trelloBoards') as string));
     }
-  }, []);
+    console.log(currentBoard);
+  }, [currentBoard]);
 
   const reloadBoard = () => {
     if (iframeRef.current) {
@@ -39,43 +42,57 @@ export default function SideBar({ isOpen, setIsOpen }: any) {
           height={27}
           alt="close window"
           className="cursor-pointer"
-          onClick={() => setIsOpen(false)}
+          onClick={() => {
+            setIsOpen(false);
+            setOpenIframe(false);
+          }}
         />
       </div>
-      <div className="h-full w-full bg-main-color flex flex-col items-center justify-between p-5 sm:justify-around">
-        <h1 className="font-semibold text-xl text-white">Ваши доски Trello:</h1>
-        <div className="flex flex-col gap-4 w-full">
-          {boards.map((board, id) => (
-            <div
-              key={id}
-              className="rounded-md p-2 px-6 text-center text-white bg-trello-color w-full"
-            >
-              {board.name}
-            </div>
-          ))}
-        </div>
-        <div className="w-full text-red-200 text-sm text-center flex flex-col gap-5">
-          <span>
-            Внимание! Не забудьте сделать вашу доску публичной, для того чтобы
-            вы могли наблюдать за изменениями в реальном времени.
-          </span>
-          <span>
-            Достаточно просто нажать на данную иконку возле названия вашей доски
-            и выбрать публичную видимость.
-          </span>
-          <span className="flex items-center justify-center">
-            <Image src="/icon.svg" alt="d" width={25} height={25} />
-          </span>
-        </div>
-        {/* <iframe
+      {openIframe ? (
+        <iframe
           ref={iframeRef}
-          src="https://trello.com/b/3e2yZkag.html"
+          src={`${currentBoard}.html`}
           width=""
           frameBorder={0}
           height=""
-          className=" border-blue-900 h-full w-full"
-        ></iframe> */}
-      </div>
+          className=" border-blue-900 h-full w-full bg-gray-400 flex items-center justify-center text-white"
+        ></iframe>
+      ) : (
+        <div className="h-full w-full bg-main-color flex flex-col items-center justify-between p-5 sm:justify-around">
+          <h1 className="font-semibold text-xl text-white">
+            Ваши доски Trello:
+          </h1>
+          <div className="flex flex-col gap-4 w-full">
+            {boards.map((board, id) => (
+              <div
+                key={id}
+                className="rounded-md p-2 px-6 text-center text-white bg-trello-color w-full cursor-pointer"
+                onClick={() => {
+                  setCurrentBoard(board.shortUrl);
+                  setOpenIframe(true);
+                }}
+              >
+                {board.name}
+              </div>
+            ))}
+          </div>
+          <div className="w-full text-red-200 text-sm text-center flex flex-col gap-5">
+            <span>
+              Внимание! Сделайте вашу доску публичной, чтобы наблюдать за
+              изменениями в реальном времени.
+            </span>
+            <span>
+              Найдите значок, как показано ниже, возле названия доски,
+            </span>
+            <span className="flex items-center justify-center">
+              <Image src="/icon.svg" alt="d" width={25} height={25} />
+            </span>
+            <span className="">
+              нажмите на него и выберите &ldquo;Публичная видимость&rdquo;.
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
