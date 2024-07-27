8ad95f795bbc7661ab5f8097f225d2d696961433
Madiyar <madikensky2003@gmail.com>
2024-07-27 13:45:26 +0500

    optimizing back

--- frontend/src/app/lib/url.tsx
+++ frontend/src/app/lib/url.tsx
@@ -1,7 +1,7 @@
 const deploy = 'https://nf-pm-ai-production.up.railway.app';
 const local = 'http://localhost:5000';
 
-const url = local;
+const url = deploy;
 
 // https://nf-pm-ai-production.up.railway.app
 // http://localhost:5000
